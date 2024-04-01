42cd5a722fb0e786c808f62f043d0bca35f7879c
Gautam Raj <gautamraj996@gmail.com>
2024-04-01 11:58:28 +0530

    refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,41 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Layout from './Layout.tsx';
+import About from './pages/About.tsx';
+import Home from './pages/Home.tsx';
+import Contact from './pages/Contact.tsx';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+import { Provider } from 'react-redux';
+import store from './app/store.ts';
+import Write from './pages/Write.tsx';
+import SinglePost from './pages/SinglePost.tsx';
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/write' element={<Write />} />
+        <Route path='/post/:id' element={<SinglePost />} />
+      </Route>
+    )
+  );
+  return (
+    <>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,40 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-import Layout from './Layout.tsx';
-import About from './pages/About.tsx';
-import Home from './pages/Home.tsx';
-import Contact from './pages/Contact.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
-import { Provider } from 'react-redux';
-import store from './app/store.ts';
-import Write from './pages/Write.tsx';
-import SinglePost from './pages/SinglePost.tsx';
+import App from './App.tsx';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/write' element={<Write />} />
-      <Route path='/post/:id' element={<SinglePost />} />
-    </Route>
-  )
-);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
