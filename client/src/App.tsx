993b17990228cf3c3c85bc08f68e496e8123c356
Gautam Raj <gautamraj996@gmail.com>
2024-04-01 12:15:43 +0530

    refactor again

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,12 +10,12 @@ import Home from './pages/Home.tsx';
 import Contact from './pages/Contact.tsx';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
-import { Provider } from 'react-redux';
-import store from './app/store.ts';
+import { useSelector } from 'react-redux';
 import Write from './pages/Write.tsx';
 import SinglePost from './pages/SinglePost.tsx';
 
 function App() {
+  const user = useSelector((store) => store.user.user);
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
@@ -24,16 +24,14 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/write' element={<Write />} />
+        <Route path='/write' element={user === null ? <Login /> : <Write />} />
         <Route path='/post/:id' element={<SinglePost />} />
       </Route>
     )
   );
   return (
     <>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
+      <RouterProvider router={router} />
     </>
   );
 }
