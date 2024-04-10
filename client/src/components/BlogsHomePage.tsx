f48726e7ea51a060db2c841366c72538d105f848
Gautam Raj <gautamraj996@gmail.com>
2024-04-10 09:56:27 +0530

    modify BlogsHomePage and Post

--- client/src/components/BlogsHomePage.tsx
+++ client/src/components/BlogsHomePage.tsx
@@ -3,11 +3,13 @@ import Heading from "./Heading";
 import Post from "./Post";
 import axios from "axios";
 import { API_URL } from "../config";
+import { Link } from "react-router-dom";
 
 interface PostData {
   coverImage: string;
   title: string;
-  createdAt: string;
+  createdAt: Date;
+  _id: string;
 }
 
 const BlogsHomePage: React.FC = () => {
@@ -32,14 +34,20 @@ const BlogsHomePage: React.FC = () => {
       <section className="mt-12">
         <Heading leftProp={"left-[49%]"} />
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 mx-auto max-w-[1368px] w-[90%]">
-          {allPosts.map((post, index) => (
-            <Post
-              key={index}
-              imgsrc={post.coverImage}
-              category=""
-              blogTitle={post.title}
-              date={post.createdAt}
-            />
+          {allPosts.map((post) => (
+            <Link to={`post/${post._id}`}>
+              <Post
+                key={post._id}
+                imgsrc={post.coverImage}
+                category=""
+                blogTitle={post.title}
+                date={new Date(post.createdAt).toLocaleString("en-In", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              />
+            </Link>
           ))}
         </div>
       </section>
--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -10,21 +10,19 @@ const Post: React.FC<PostProps> = (props) => {
   const { imgsrc, blogTitle, date } = props;
 
   return (
-    <div className='mb-2'>
-      <div className='h-[200px] md:max-w-[350px] max-w-[100%] '>
+    <div className="mb-2 md:max-w-[350px] max-w-[100%] ">
+      <div className="h-[200px] md:max-w-[350px] max-w-[100%] ">
         <img
           src={imgsrc}
-          alt=''
-          className='h-[100%] w-[100%] rounded-lg object-cover'
+          alt=""
+          className="h-[100%] w-[100%] rounded-lg object-cover"
         />
       </div>
-      <p className='category text-[--another-light-color] uppercase font-[Lora] tracking-[1px]'>
-        Law
+      <p className="category text-[--another-light-color] uppercase font-[Lora] tracking-[1px]"></p>
+      <p className="blog-title font-[Unna] text-2xl font-semibold text-center">
+        {blogTitle.length > 30 ? blogTitle.slice(0, 30) + "..." : blogTitle}
       </p>
-      <p className='blog-title font-[Unna] text-2xl font-semibold'>
-        {blogTitle}
-      </p>
-      <p className='date tracking-[3px]'>{date}</p>
+      <p className="date tracking-[2px] text-center text-xs">{date}</p>
     </div>
   );
 };
