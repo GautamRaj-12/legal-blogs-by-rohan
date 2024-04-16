3897f6ff1df19cf87ecd4063628bb36d85b33451
Gautam Raj <gautamraj996@gmail.com>
2024-04-16 21:45:34 +0530

    add ts in app.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,29 +3,29 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-} from 'react-router-dom';
-import Layout from './Layout.tsx';
-import About from './pages/About.tsx';
-import Home from './pages/Home.tsx';
-import Contact from './pages/Contact.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
-import { useSelector } from 'react-redux';
-import Write from './pages/Write.tsx';
-import SinglePost from './pages/SinglePost.tsx';
+} from "react-router-dom";
+import Layout from "./Layout.tsx";
+import About from "./pages/About.tsx";
+import Home from "./pages/Home.tsx";
+import Contact from "./pages/Contact.tsx";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
+import { useSelector } from "react-redux";
+import Write from "./pages/Write.tsx";
+import SinglePost from "./pages/SinglePost.tsx";
 
 function App() {
-  const user = useSelector((store) => store.user.user);
+  const user = useSelector((store: any) => store.user.user);
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<Layout />}>
-        <Route path='' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/write' element={user === null ? <Login /> : <Write />} />
-        <Route path='/post/:id' element={<SinglePost />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/write" element={user === null ? <Login /> : <Write />} />
+        <Route path="/post/:id" element={<SinglePost />} />
       </Route>
     )
   );
