3500aa69c0a303eae0f12a0540d654f1a1aa3d9a
Gautam Raj <gautamraj996@gmail.com>
2024-04-23 10:28:02 +0530

    add cors env variabe

--- server/src/app.js
+++ server/src/app.js
@@ -4,20 +4,20 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// );
-// app.use(cors());
-
 app.use(
   cors({
-    origin: "https://legal-blogs-by-rohan.onrender.com/",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
+// app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: "https://legal-blogs-by-rohan.onrender.com/",
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
