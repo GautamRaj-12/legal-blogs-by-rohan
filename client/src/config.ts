602cd86459274446a4472da82091d65e5b3b9b2b
Gautam Raj <gautamraj996@gmail.com>
2024-04-24 14:15:31 +0530

    Api url correction

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000/api/v1";
-export const API_URL_D = "https://lionfish-app-ue4we.ondigitalocean.app/api/v1";
+export const API_URL_LOCAL = "http://localhost:8000/api/v1";
+export const API_URL = "https://lionfish-app-ue4we.ondigitalocean.app/api/v1";
