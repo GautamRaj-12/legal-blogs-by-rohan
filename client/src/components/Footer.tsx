4949683dd322a65d61df18bdfd4ef2143fbac27a
Gautam Raj <gautamraj996@gmail.com>
2024-04-27 11:12:00 +0530

    add footer contents

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,12 @@
 const Footer = () => {
+  const date = new Date();
   return (
     <>
-      <div className='bg-[--button-bg-color] w-full p-6'>
-        <p>Footer</p>
+      <div className="bg-slate-700 bg-opacity-40 w-full p-2">
+        <div className="text-lg flex  flex-col justify-center items-center font-semibold">
+          <p>Legal Blogs By Rohan</p>
+          <p>&#169; {date.getFullYear()}</p>
+        </div>
       </div>
     </>
   );
