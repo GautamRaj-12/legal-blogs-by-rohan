b24e76677be0fc0efdb8c6986a95268249d03a5d
Gautam Raj <gautamraj996@gmail.com>
2024-06-02 11:39:25 +0530

    Add hero image absolute url

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       backgroundImage: {
         heroImage:
-          "linear-gradient(to bottom right,rgba(239,99,82,0.5),rgba(38, 37, 34,0.6),rgba(159,220,38,0.3)),url('./src/assets/images/hero.webp')",
+          "linear-gradient(to bottom right,rgba(239,99,82,0.5),rgba(38, 37, 34,0.6),rgba(159,220,38,0.3)),url('https://images.unsplash.com/photo-1589216532372-1c2a367900d9?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
     },
     colors: {
