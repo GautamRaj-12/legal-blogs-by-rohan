4243e08f530be9a9b6c4c3680fb298c3618e72f0
Gautam Raj <gautamraj996@gmail.com>
2024-06-02 16:12:40 +0530

    Modify hero heading size for small screen

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <section className="bg-heroImage bg-no-repeat bg-cover bg-center h-[36rem] mt-4 rounded-[32px] flex flex-col justify-center items-center">
       <div className="max-w-[54rem] flex flex-col justify-center items-center">
-        <h1 className="text-light text-headerOne text-center">
+        <h1 className="text-light sm:text-headerOne text-headerTwo text-center">
           Beyond the Courtroom
         </h1>
         <h2 className="max-w-[26rem] text-paraOne text-light text-center mt-3 font-roboto">
